a731747b8234063ea097f9ec7cdb8ac1cc61a6fa
Kin Wong <beewong2014@hotmail.com>
2024-05-04 02:49:07 +0100

    colour for the mute button

--- src/MuteButton.jsx
+++ src/MuteButton.jsx
@@ -10,8 +10,8 @@ export default function MuteButton({ isGlobalMuted, toggleGlobalMute }) {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
+              strokeWidth={2}
+              stroke="#F78888"
               className="w-6 h-6"
             >
               <path
@@ -25,8 +25,8 @@ export default function MuteButton({ isGlobalMuted, toggleGlobalMute }) {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
+              strokeWidth={2}
+              stroke="#50f3a9"
               className="w-6 h-6"
             >
               <path
